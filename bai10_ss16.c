6da6cf84d667d1ec4e3abfa16bf33bed94155675
NguyenThiYen03032006 <nguyenthiyen03032006@gmail.com>
2024-12-17 09:38:28 +0700

    bai10_ss16.c

--- bai10_ss16.c
+++ bai10_ss16.c
@@ -1,27 +1,51 @@
 #include<stdio.h>
-void deleteArr(int *arr, int index);
+#include<stdlib.h>
+// xay dung ham them phan tu
+int deleteElement(int *arr, int index, unsigned long *n);
+// cu phap realloc  : arr= realloc(arr,(*n-1) *sizeof(int))
 int main(void){
-    int arr[]={1,2,3,4,5};
-    int length=sizeof(arr)/ sizeof(int);
-    for(int i=0; i<length; i++){
-        printf("%d ", arr[i]);
+    int *arr;
+    unsigned long n;
+    int index=0;
+    printf("moi ban nhap so luong phan tu: ");
+    scanf("%lu", &n);
+    // cap phat bo nho =malloc gom n phan tu
+    arr=(int *)malloc(n* sizeof(int)); // int arr[n] cx dc
+    for(int i=0; i<n; i++){
+        printf("Nhap phan tu thu %d: ", i+1);
+        scanf("%d", (arr +i));// = &arr[i]
     }
-    int index;
-    printf("\nMoi ban nhap vi tri muon xoa: ");
+    printf("Moi ban nhap dia chi muon xoa: ");
     scanf("%d", &index);
-    if( index<0 || index >= length){
-        printf("Vi tri ban muon xoa khong hop le \n");
-        return 1;
+    // goi ham xoa phan tu
+    deleteElement(arr, index, &n);
+    // in ra cac phan tu
+    for(int i=0; i<n; i++){
+        printf("%d \t", *(arr+i));// arr[i] cx dc
     }
-    deleteArr(arr, index);
-    for(int i=0; i<length-1; i++){
-        printf("%d ", arr[i]);
-    }
+    
     printf("\n");
+    
     return 0;
 }
-void deleteArr(int *arr, int index){
-    for(int i=index; i<4; i++){
-        *(arr+i)=*(arr +i+1);
+// di trien khai ham
+int deleteElement(int *arr, int index, unsigned long *n){
+    // truoc khi xoa phai ktra xem index thoa man khong
+    if(index<0 || index> *n){
+        printf("Vi tri xoa khong hop le \n");
+        return 0;
     }
+    // vi tri xoa thoa man
+    // tien hanh dich chuyen phan tu
+    for(int i= index; i<*n; i++){
+        *(arr+i)=*(arr+i+1);
+    }
+    // giam bo nho cho mang
+
+    arr= realloc(arr,(*n -1) *sizeof(int));
+        (*n)--; // tang kich thuoc len 1
+    
+    return index;
 }
+
+
